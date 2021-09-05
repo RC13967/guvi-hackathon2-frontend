a241c0a06b569584cd506b8bea59a1bf774a8dff
ranjith <ranjithch137@gmail.com>
2021-09-05 14:26:54 +0530

    k

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import { Link, Route, Switch,  useParams } from "react-router-dom";
+import { Link, Route, Switch, useParams } from "react-router-dom";
 import { createContext, useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 const moviescontext = createContext(null);
+const usercontext = createContext(null);
 function App() {
   const [movies, setMovies] = useState([]);
   const [username, setUsername] = useState("");
@@ -20,6 +21,7 @@ function App() {
   );
 }
 function Routes() {
+  const [user,setUser] = useState([]);
   return (
     <Switch>
       <Route path="/admin">
@@ -32,7 +34,12 @@ function Routes() {
         <Bookticket />
       </Route>
       <Route path="/crudtheatre">
+        <usercontext.Provider value={{user:user,setUser:setUser}} >
         <Crudtheatre />
+        </usercontext.Provider>
+      </Route>
+      <Route path="/edithall/:id">
+        <Edithall />
       </Route>
       <Route path="/">
         <Home />
@@ -66,42 +73,40 @@ function Admin() {
 
 }
 function Crudtheatre() {
-  const [user, setUser] = useState([]);
-  const { username, password} = useContext(moviescontext);
-  function getusers(){
+  const history = useHistory();
+  const {user, setUser} = useContext(usercontext);
+  const { username, password } = useContext(moviescontext);
+  function getusers() {
     fetch(`https://guvi-hackathon2-ranjith.herokuapp.com/users/${username}`, {
       method: "GET"
     })
       .then((data) => data.json())
-      .then((userdata)=> setUser(userdata));
+      .then((userdata) => setUser(userdata));
   }
   useEffect(() => {
     getusers();
   }, []);
-  if(user[0] === undefined || user[0].halls === undefined || password !== user[0].password){
-    return(
+  if (user[0] === undefined || user[0].halls === undefined || password !== user[0].password) {
+    return (
       <>
-      Invalid credentials
+        Invalid credentials
       </>
     )
   }
-  
-  return(
+
+  return (
     <div className="halls-container">
-    {user[0].halls.map(({adress, hallname, title,id})=>
-    <div className="hall-container">
-      <div className="hall-name">{hallname}</div>
-      <div className="movie-title">{title}</div>
-      <div className="hall-adress">{adress}</div>
-      <button onClick = {()=>edithall(adress, hallname,title,id)}>Edit hall</button>
-      <button onClick = {()=>deletehall(id)}>Delete hall</button>
-    </div>
-    )} 
+      {user[0].halls.map(({ adress, hallname, title, id }) =>
+        <div className="hall-container">
+          <div className="hall-name">{hallname}</div>
+          <div className="movie-title">{title}</div>
+          <div className="hall-adress">{adress}</div>
+          <button onClick={() => history.push("edithall" + id)}>Edit hall</button>
+          <button onClick={() => deletehall(id)}>Delete hall</button>
+        </div>
+      )}
     </div>
   )
-  function edithall(hallname){
-return
-  }
 
   function deletehall(id) {
     fetch(`https://guvi-hackathon2-ranjith.herokuapp.com/hall/${id}`, {
@@ -109,13 +114,50 @@ return
       headers: {
         "Content-Type": "application/json"
       },
-      body:JSON.stringify({username:username})
+      body: JSON.stringify({ username: username })
     })
       .then((data) => data.json())
-      .then(()=>getusers())
+      .then(() => getusers())
   }
 }
 
+function Edithall() {
+  const {user, setUser} = useContext(usercontext);
+  const {username} = useContext(moviescontext);
+  const { id } = useParams();
+  let [{ adress, hallname, title }] = user[0].halls.filter((hall) => hall.id === id);
+  const [Adress, setAdress] = useState(adress);
+  const [Title, setTitle] = useState(hallname);
+  const [Hallname, setHallname] = useState(title);
+  function edithall() {
+    fetch(`https://guvi-hackathon2-ranjith.herokuapp.com/edithall/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ hallname: Hallname, adress: Adress, title: Title })
+    })
+      .then((data) => data.json())
+      .then(() => getusers())
+  }
+  function getusers() {
+    fetch(`https://guvi-hackathon2-ranjith.herokuapp.com/users/${username}`, {
+      method: "GET"
+    })
+      .then((data) => data.json())
+      .then((userdata) => setUser(userdata));
+  }
+  useEffect(() => {
+    edithall();
+  }, []);
+  return (
+    <div>
+      <input type="text" placeholder={Hallname} onChange={(event) => setHallname(event.target.value)} />
+      <input type="text" placeholder={Title} onChange={(event) => setTitle(event.target.value)} />
+      <input type="text" placeholder={Adress} onChange={(event) => setAdress(event.target.value)} />
+    </div>
+  )
+}
 function Client() {
   const history = useHistory();
   const { movies, setMovies } = useContext(moviescontext);
@@ -128,13 +170,13 @@ function Client() {
   }
   useEffect(() => {
     getmovies();
-  }, );
+  });
 
   return (
     <div className="movies-container" >
       {movies.map(({ poster, title, genres }) =>
         <div className="movie-card" >
-          <img src={poster} alt = ""/>
+          <img src={poster} alt="" />
           <div className="movie-title">{title}</div>
           <div className="movie-genre">{genres}</div>
           <button className="pointer" onClick={() => history.push("/book/" + title)}>Book</button>
