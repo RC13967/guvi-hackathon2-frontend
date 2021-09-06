99e477a930c571394d26da33951daba8332ac103
ranjith <ranjithch137@gmail.com>
2021-09-07 00:33:42 +0530

    k

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ function Admin() {
   const [buttonText, setButtonText] = useState("Login");
   const { username, password, setUsername, setPassword, setUser } = useContext(moviescontext);
   function adduser() {
+
     fetch(`https://guvi-hackathon2-ranjith.herokuapp.com/addUser`, {
       method: "POST",
       headers: {
@@ -103,8 +104,9 @@ function Crudtheatre() {
   const history = useHistory();
   const { username, password, user, setUser, setnewHallname, setnewTitle, setnewAdress } = useContext(moviescontext);
   function getusers() {
-    fetch(`https://guvi-hackathon2-ranjith.herokuapp.com/users/${username}`, {
-      method: "GET"
+    fetch(`https://guvi-hackathon2-ranjith.herokuapp.com/user`, {
+      method: "GET",
+      body: JSON.stringify({ username: username, password:password })
     })
       .then((data) => data.json())
       .then((userdata) => setUser(userdata));
